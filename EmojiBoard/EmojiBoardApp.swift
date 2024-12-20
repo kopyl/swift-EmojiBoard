15190caeda17bc54bedaa2ff3779631a54226001
kopyl <oleggggg1997@gmail.com>
2024-12-20 15:27:24 +0200

    Initial Commit

---
+++ EmojiBoard/EmojiBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiBoardApp.swift
+//  EmojiBoard
+//
+//  Created by Oleh Kopyl on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
